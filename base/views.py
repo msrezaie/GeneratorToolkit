087c280dec67e2952d7c5a27e1721ec0d5c5930e
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-03-01 12:54:39 -0500

    basic structure given to base app
    apps details in the base page are fetched from the apps model

--- base/views.py
+++ base/views.py
@@ -1,5 +1,12 @@
 from django.views.generic import TemplateView
+from . models import App
 
 
 class Base(TemplateView):
     template_name = 'base/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        apps = App.objects.all()
+        context['apps'] = apps
+        return context
