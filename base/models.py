c8b7a408911db1a6877d26f2148e4bf96a997240
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-03-01 12:48:30 -0500

    apps model created
    a model for storing necessary information about all the created apps within the project

--- base/models.py
+++ base/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class App(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True, blank=True)
+    url_name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
