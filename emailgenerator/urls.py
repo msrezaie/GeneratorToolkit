917dbff492b1036438e287f9e171a096f850cb2c
Mohammad Rezaie <79583632+msrezaie@users.noreply.github.com>
2023-03-10 12:34:32 -0500

    Email generator app deleted

--- emailgenerator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from emailgenerator.views import EmailGenerator
-
-app_name = 'emailgenerator'
-
-urlpatterns = [
-    path('', EmailGenerator.as_view(), name='emailgenerator'),
-]
